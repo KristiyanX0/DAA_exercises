54e209f4001d4d8667d89f02298d758c3400ee42
Kristiyan Semedzhiev <kristiyan.semerdjiev@gmail.com>
2022-04-25 00:32:01 +0300

    knights.cpp almost ready

--- DFS_BFS_exercises/knights.cpp
+++ DFS_BFS_exercises/knights.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-#define SIZE 2000
+#include <queue>
+#define SIZE 200
+#define VISITED 1
+
 int N, M, K;
+bool visited[SIZE][SIZE]={false};
+int dist[SIZE][SIZE]={0};
 
 std::pair<int,int> validMovements[] = {{-1,-2},{-2,-1},{-2,+1},{-1,+2},
                                        {+1,+2},{+2,+1},{+2,-1},{+1,-2}};
@@ -9,24 +14,51 @@ bool isValidPos(const std::pair<int,int> &node) {
     return node.first>=0 && node.first<N && node.second>=0 && node.second<N;
 }
 
+void BFS(std::pair<int,int> x) {
+    visited[x.first][x.second] = true;
+    std::queue<std::pair<int,int>> qu;
+    qu.push(x);
+    while(!qu.empty()) {
+        std::pair<int,int> curr = qu.front();
+        qu.pop();
+        for (int i=0;i<8;++i) {
+            std::pair<int,int> neighbour = {curr.first + validMovements[i].first,
+                                            curr.second + validMovements[i].second};
+            if (isValidPos(neighbour) && !visited[neighbour.first][neighbour.second]) {
+                qu.push(neighbour);
+                dist[neighbour.first][neighbour.second] = dist[curr.first][curr.second] + 1;
+                visited[neighbour.first][neighbour.second] = true;
+            }
+        }
+    }
+}
+
 int main() {
-    int eflandia[SIZE][SIZE]={0};
     std::pair<int,int> knights[SIZE];
     std::pair<int,int> princess;
     scanf("%d %d %d",&N,&M,&K);
     int x,y;
-    for (size_t i=0;i<N;++i) {
-        scanf("%d %d",&x,&y);
-        eflandia[x][y] = {-1};
-    }
     for (size_t i=0;i<M;++i) {
         scanf("%d %d",&x,&y);
-        princess.first=x;
-        princess.second=y;
+        visited[x][y] = VISITED;
+    }
+    for (size_t i=0;i<K;++i) {
+        scanf("%d %d",&x,&y);
+        knights[i].first;
+        knights[i].second;
     }
     scanf("%d %d",&x,&y);
     princess.first=x;
     princess.second=y;
-    
+    BFS(princess);
+    int maxSteps = 0;
+    int countOfKnights = 0;
+    for (size_t i = 0; i < K; ++i) {
+        if (visited[knights[i].first][knights[i].second]) {
+            ++countOfKnights;
+            maxSteps=std::max(maxSteps,dist[knights[i].first][knights[i].second]);
+        }
+    }
+    std::cout << countOfKnights  << " " << maxSteps;
     return 0;
 }
