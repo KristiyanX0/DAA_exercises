9be5e7f355515c6bdd8c841aaefe6203381f29c5
Kristiyan Semedzhiev <kristiyan.semerdjiev@gmail.com>
2022-04-25 22:40:38 +0300

    fixed no11C.cpp

--- DFS_BFS_exercises/no11C.cpp
+++ DFS_BFS_exercises/no11C.cpp
@@ -1,11 +1,10 @@
 // https://judge.openfmi.net/practice/get_problem_description?contest_id=178&problem_id=554
-//UNFINISHED
 
 #include<iostream>
 #include<vector>
 #include<queue>
 #define SIZE 1009
-#define Qsize 10009
+#define Qsize 100009
 
 std::vector<int> path[SIZE];
 bool connections[SIZE][SIZE]={false};
