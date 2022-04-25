33ff1f209b455d51ecb5edf58f86bb830557c07d
Kristiyan Semedzhiev <kristiyan.semerdjiev@gmail.com>
2022-04-25 03:34:18 +0300

    almostDone

---
+++ DFS_BFS_exercises/no11C.cpp
@@ -0,0 +1,58 @@
+// https://judge.openfmi.net/practice/get_problem_description?contest_id=178&problem_id=554
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#define SIZE 1000
+#define Qsize 10000
+
+std::vector<int> path[SIZE];
+bool connections[SIZE][SIZE]={false};
+int curr = -1;
+
+void BFS(int x) {
+    bool visited[SIZE] = {false};
+    std::queue<int> qu;
+
+    visited[x] = true;
+    qu.push(x);
+    while (!qu.empty()) {
+        x = qu.front();
+        connections[curr][x]=true;
+        qu.pop();
+        int size = path[x].size();
+        for (size_t i = 0; i < size; ++i) {
+            if (!visited[path[x][i]]) {
+                visited[path[x][i]] = true;
+                qu.push(path[x][i]);
+            }
+        }
+    }
+}
+
+
+int main() {
+    int N,M,Q;
+    scanf("%d %d",&N,&M);
+    int x,y;
+    for (size_t i=0;i<M;++i) {
+        scanf("%d %d",&x,&y);
+        path[x].push_back(y);
+        path[y].push_back(x);
+    }
+    for (size_t i=1;i<=N;++i) {
+        if (!path[i].empty()) {
+            curr=i;
+            BFS(i);
+        }
+    }
+    scanf("%d",&Q);
+    std::vector<bool> QQ(Qsize);
+    for (size_t i=0;i<Q;++i) {
+        scanf("%d %d",&x,&y);
+        QQ[i]=connections[x][y];
+    }
+    for (size_t i=0;i<Q;++i)
+        printf("%d",(int)QQ[i]);
+    return 0;
+}
--- DFS_BFS_exercises/skola.cpp
+++ DFS_BFS_exercises/skola.cpp
@@ -9,10 +9,6 @@ void addEdge(int x, int y, std::vector<int> (&students)[SIZE]) {
     students[x].push_back(y);
 }
 
-void BFS(int x, std::vector<int> (&students)[SIZE]) {
-
-}
-
 int main() {
     int N;
     scanf("%d",&N);
@@ -21,6 +17,5 @@ int main() {
     while (scanf("%d %d",&x,&y) != EOF) {
         addEdge(x,y,students);
     }
-    BFS(0,students);
     return 0;
 }
