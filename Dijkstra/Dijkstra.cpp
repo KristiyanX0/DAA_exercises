6cca2a24180ea8df3f4f01561f04e1f660ddec0f
Kristiyan Semedzhiev <kristiyan.semerdjiev@gmail.com>
2022-05-19 12:44:37 +0300

    updated dijkstra

--- Dijkstra/Dijkstra.cpp
+++ Dijkstra/Dijkstra.cpp
@@ -1,14 +1,11 @@
-#include<cstdio>
-#include<vector>
-#include<queue>
-#include<limits.h>
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <vector>
+#define SIZE (int)1e5
 
-using namespace std;
-
-int n, m;
-int parent[int(1e5)];
-// int pathsCount[int(1e5)];
-vector<pair<int, int>> gr[int(1e5)];
+int n,m;
+std::vector<std::pair<int,int>> gr[SIZE];
 
 void init() {
     int from,to,weight;
@@ -21,48 +18,51 @@ void init() {
 }
 
 void dijkstra(int src) {
-    
-    int dist[int(1e5)];
-    bool used[int(1e5)];
+    int dist[SIZE];
+    bool used[SIZE]={false};
+    int parent[SIZE];
     std::fill(dist,dist+n,INT_MAX);
-    std::priority_queue<std::pair<int,int>/*{node,weight}*/, 
-                        std::vector<std::pair<int,int>>, 
-                        std::greater<std::pair<int,int>>> nodes;
-    nodes.push({0,src});
+    std::priority_queue<std::pair<int,int>,std::vector<std::pair<int,int>>,std::greater<std::pair<int,int>>> nodes;
+    nodes.push({0,src}); // първото е нула защото трябва да се сортира по 0
     dist[src]=0;
     parent[src]=-1;
-    while(!nodes.empty()) {
-        int vertex = nodes.top().second;
+    while (!nodes.empty()) {
+        int vertex=nodes.top().second;
         nodes.pop();
         if (used[vertex])
             continue;
         used[vertex]=true;
-
         for (auto x : gr[vertex]) {
-            int v = x.first;
+            int next_vertex = x.first;
             int weight = x.second;
-            int newDist = dist[vertex] + weight;
-            if (dist[v] > newDist) {
-                parent[v]=vertex;
-                dist[v] = newDist;
-                nodes.push({dist[v],v});
+            int newDist = dist[vertex]+weight;
+            if (dist[next_vertex]>newDist) {
+                dist[next_vertex]=newDist;
+                parent[next_vertex]=vertex;
+                nodes.push({dist[next_vertex],next_vertex});
+
             }
         }
     }
-    printf("\nThe source is %d\nVertices:\tDistance:\tPath:\n",src);
+    printf("Source: %d\tVertices\tPath\n",src);
     for (int i=0;i<n;++i) {
-        printf("%d\t\t%d\t\t",i,dist[i]);
-        int x=i;
-        while(x!=-1){
-            printf("%d <- ",x);
-            x=parent[x];
+        printf("%d\t\t",i);
+        if (dist[i] != INT_MAX) {
+            printf("%d\t\t",dist[i]);
+            int x=i;
+            while(x!=-1){
+                printf("%d <- ",x);
+                x=parent[x];
+            }
         }
+        else
+            printf("inf\t\t");
         printf("\n");
     }
 }
 
-int main(){
+int main() {
     init();
-    dijkstra(0);
+    dijkstra(5);
     return 0;
 }
